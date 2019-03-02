89e6c20e6f8a34cdd0f49c2d5f889d1b8f329c1f
Stephan <44555785+stdhd@users.noreply.github.com>
2019-03-02 17:05:28 +0100

    update indices

--- bomberman_environment/indices.py
+++ bomberman_environment/indices.py
@@ -1,3 +1,5 @@
+
+
 import numpy as np
 
 def x_y_to_index(x, y, ncols, nrows):
@@ -12,7 +14,7 @@ def x_y_to_index(x, y, ncols, nrows):
     :return: Index of square x, y coords point to
     """
 
-    if x >= ncols or x <= 0 or y >= nrows or y <= 0:
+    if x >= ncols - 1 or x <= 0 or y >= nrows - 1 or y <= 0:
         raise ValueError("Coordinates outside of game grid")
     if (x + 1) * (y + 1) % 2 == 1:
         raise ValueError("Received wall coordinates!")
@@ -30,7 +32,7 @@ def x_y_to_index(x, y, ncols, nrows):
 
         return int(ind)
 
-    
+
 def index_to_x_y(ind, ncols, nrows):
     """
     Convert a given coordinate index into its x, y representation. Indices start at 1 !
@@ -42,18 +44,22 @@ def index_to_x_y(ind, ncols, nrows):
 
     y = 1
 
-    while True:
-        # Volle Reihe
-        if ind <= ncols - 2:
-            x = ind
-            return x, y
+    full = True
+
+    while ind > ncols - 2 and full == True:
         ind -= ncols - 2
         y += 1
-        
-        # Halbe Reihe
-        if ind <= ((ncols - 2) // 2 + 1):
-            x = 2 * ind - 1
-            return x, y
-        ind -= ((ncols - 2) // 2 + 1)
-        y += 1
+        full = False
 
+        while ind > np.ceil((ncols - 2) / 2) and full == False:
+            ind -= np.ceil((ncols - 2) / 2)
+            y += 1
+            full = True
+
+    if full:
+        x = ind
+
+    else:
+        x = 2 * ind - 1
+
+    return int(x), int(y)

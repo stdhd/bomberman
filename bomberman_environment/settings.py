c284a00933a97e24f4b7fa902444999b2162cb6e
Eike Harms <eike.harms@sap.com>
2019-03-12 15:54:34 +0100

    Add merged agent for collecting coins

--- bomberman_environment/settings.py
+++ bomberman_environment/settings.py
@@ -9,13 +9,13 @@ settings = {
     # Display
     'width': 1000,
     'height': 600,
-    'gui': False,
+    'gui': True,
     'fps': 15,
 
     # Main loop
-    'update_interval': 0.3, # 0.33,
+    'update_interval': 0.1, # 0.33,
     'turn_based': False,
-    'n_rounds': 1000,
+    'n_rounds': 100,
     'save_replay': False,
     'make_video_from_replay': False,
 

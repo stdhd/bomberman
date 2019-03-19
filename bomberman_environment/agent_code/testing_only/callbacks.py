6f3fe7dffe6647adbdb13bc43315a9041b09b2e9
Stephan <44555785+stdhd@users.noreply.github.com>
2019-03-19 21:38:31 +0100

    add more data

--- bomberman_environment/agent_code/testing_only/callbacks.py
+++ bomberman_environment/agent_code/testing_only/callbacks.py
@@ -16,11 +16,14 @@ def setup(self):
     You can also use the self.logger object at any time to write to the log
     file for debugging (see https://docs.python.org/3.7/library/logging.html).
     """
-    self.learning_rate = 0.4
-    self.discount = 0.7
-    self.epsilon = -1
     self.train_flag = False
-    self.obs_object = ObservationObject(1, ['d_closest_coin_dir', 'd_closest_safe_field_dir', 'me_has_bomb'], None)
+    self.obs_object = ObservationObject(1, ['d_closest_coin_dir',
+                                'd_closest_safe_field_dir',
+                                'me_has_bomb',
+                                'd4_is_safe_to_move_a_l',
+                                'd4_is_safe_to_move_b_r',
+                                'd4_is_safe_to_move_c_u',
+                                'd4_is_safe_to_move_d_d'], None)
     # Used for plotting
     self.total_steps_over_episodes = 0
     self.total_deaths_over_episodes = 0
@@ -31,6 +34,7 @@ def setup(self):
     filename = self.obs_object.get_file_name_string()
 
     try:
+        print(os.path.join('data', 'qtables', filename, 'q_table-' + filename + '.npy'))
         self.q_table = np.load(os.path.join('data', 'qtables', filename, 'q_table-' + filename + '.npy'))
         self.quantities = np.load(os.path.join('data', 'qtables', filename, 'quantity-' + filename + '.npy'))
         self.logger.debug('LOADED Q')
@@ -66,81 +70,33 @@ def act(self):
     self.next_action = 'WAIT'
 
     # Gather information about the game state
-    # bomb_xys = [(x,y) for (x,y,t) in bombs]
-    # others = [(x,y) for (x,y,n,b,s) in self.game_state['others']]
-
-    arena = self.game_state['arena']
     x, y, _, bombs_left, score = self.game_state['self']
-    bombs = self.game_state['bombs']
-    # self.logger.info(f'BOMBS: {bombs}')
-    rep = derive_state_representation(self)  # DEBUG
+    rep = derive_state_representation(self)
     self.obs_object.set_state(rep)
     observation = self.obs_object.create_observation(np.array([0]))[0]
-    # observation = np.delete(observation, [0])
     self.old_observation = observation
-    # self.logger.info(f'self: {[x, y]}')
-    # self.logger.info(f'Observation: {observation}')
-    
+    observation_ind = np.where((self.observation_db == observation).all(axis=1))[0]
+    # If observation_db has entry the action with the highest value is chosen.
+    if observation_ind.shape[0] != 0:
+        print("---")
+        print("KNOWN observation", self.obs_object.get_file_name_string())
+        print(self.observation_db[observation_ind[0]])
+        # print(self.observation_db[observation_ind])
+        print('LEFT', 'RIGHT', 'UP', 'DOWN', 'WAIT', 'BOMB')
+        print(self.q_table[observation_ind[0]])
+        print("Quantities: ")
+        print(self.quantities[observation_ind[0]])
+        self.last_action_ind = np.random.choice(
+            np.flatnonzero(self.q_table[observation_ind[0]] == self.q_table[observation_ind[0]].max()))
 
-    # Search for state in observation_db
-    # If/else needed because np.where can only be done if self.observation_db is not empty
-    if self.observation_db.shape[0] != 0:
-        warnings.simplefilter(action='ignore', category=FutureWarning)
-        observation_ind = np.where((self.observation_db == np.array([observation])).all(axis=1))[0]
-       # np.where((KNOWN == np.array([observation])).all(axis=1))[0]
-        self.last_q_ind = observation_ind
+    # If test mode and observation_db has no entry
     else:
-        observation_ind = np.array([])
-
-    # If Zufällig und training
-        # random action
-        # update q_table and observation_db
-        # IF observation_db hat keinen Eintrag
-            # append q_table and observation_db
-    # If Nicht zufällig
-        # If observation_db has no entry and training
-            # random action
-            # q_table and observation_db
-        # ElIf observation_db has entry
-            # argmax action
-        # Else
-            # random action
-
-    if self.epsilon > np.random.uniform(0,1) and self.train_flag:
-        self.last_action_ind = np.random.randint(0,6)
-        if observation_ind.shape[0] == 0:
-            self.observation_db = np.append(self.observation_db, np.array([observation]), axis = 0)
-            self.q_table = np.append(self.q_table, np.zeros([1, self.q_table.shape[1]]), axis = 0)
-            self.last_q_ind = self.q_table.shape[0] - 1
-    else:
-        # If training mode is on and observation_db has no entry it has to be appended and a random action is chosen.
-        if observation_ind.shape[0] == 0 and self.train_flag:
-            self.last_action_ind = np.random.randint(0,6)
-            self.observation_db = np.append(self.observation_db, np.array([observation]), axis = 0)
-            self.q_table = np.append(self.q_table, np.zeros([1, self.q_table.shape[1]]), axis = 0)
-            self.last_q_ind = self.q_table.shape[0] - 1
-        # If observation_db has entry the action with the highest value is chosen.
-        elif observation_ind.shape[0] != 0:
-            print("---")
-            print("KNOWN observation", self.obs_object.get_file_name_string())
-            print(self.observation_db[observation_ind[0]])
-            # print(self.observation_db[observation_ind])
-            print('LEFT', 'RIGHT', 'UP', 'DOWN', 'WAIT', 'BOMB')
-            print(self.q_table[observation_ind[0]])
-            print("Quantities: ")
-            print(self.quantities[observation_ind[0]])
-            self.last_action_ind = np.random.choice(np.flatnonzero(self.q_table[observation_ind[0]] == self.q_table[observation_ind[0]].max()))
-
-        # If test mode and observation_db has no entry
-        else:
-            # TODO: regression
-            print("unknown observation")
-            print(observation)
-            self.last_action_ind = np.random.randint(0,6)
+        # TODO: regression
+        print("unknown observation")
+        print(observation)
+        self.last_action_ind = np.random.randint(0, 6)
 
     self.next_action = ['LEFT', 'RIGHT', 'UP', 'DOWN', 'WAIT', 'BOMB'][self.last_action_ind]
-   # if self.total_steps_over_episodes == 0:
-    #    self.next_action = 'UP'
     print(self.next_action)
 
 def reward_update(self):
